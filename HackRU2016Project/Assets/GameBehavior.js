1073ce117e1f701223d2b7566a364099b0e1a864
bharddwaj <bharddwajvemulapalli@gmail.com>
2016-04-17 10:58:09 -0400

    Unity commit

---
+++ HackRU2016Project/Assets/GameBehavior.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+static var playerScore1: int = 0;
+static var playerScore2 : int = 0;
+
+var theSkin : GUISkin;
+
+static function Score (wallName : String) {
+	if(wallName=="rightWall"){
+	playerScore1++;
+	}
+	else{
+	playerScore2++;
+	}
+	Debug.Log("Player 1 Score is " + playerScore1);
+	Debug.Log("Player 2 Score is " + playerScore2);
+}
+function OnGUI(){
+	GUI.skin=theSkin;
+	GUI.Label(new Rect(Screen.width/2-150-12,20,100,100), "" + playerScore1);
+	GUI.Label(new Rect(Screen.width/2+150-12,20,100,100), "" + playerScore2);
+	}
---
+++ HackRU2016Project/Assets/Player1.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var moveUp : KeyCode;
+
+var moveDown : KeyCode;
+
+var moveLeft: KeyCode;
+
+var moveRight: KeyCode;
+
+var movement : float = 10;
+
+function Update () {
+
+ if (Input.GetKey(moveUp)){
+
+  GetComponent.<Rigidbody2D>().velocity.y = movement;
+
+ }
+
+ else if (Input.GetKey(moveDown)) {
+
+  GetComponent.<Rigidbody2D>().velocity.y = -movement;
+ }
+
+ else if (Input.GetKey(moveLeft)){
+
+  GetComponent.<Rigidbody2D>().velocity.x = -movement;
+ }
+
+ else if (Input.GetKey(moveRight)){
+
+  GetComponent.<Rigidbody2D>().velocity.x = movement;
+ 
+ }
+
+ }
